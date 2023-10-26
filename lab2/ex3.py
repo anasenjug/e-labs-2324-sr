7d54153cfa7973154621dbb7839c5183acb98e2f
LuQhor <LuQchor26@gmail.com>
2023-10-26 09:33:26 +0200

    modified ex3.py for json dumping

--- lab2/ex3.py
+++ lab2/ex3.py
@@ -1,9 +1,12 @@
 import csv
+import json
+
 from csv import DictReader
 
 with open('ex2-text.csv', 'r') as file:
     dict_reader=DictReader(file)
     employees=list(dict_reader)
-    print(employees)
 file.close()
 
+with open('ex4-employees.json', 'w', encoding='utf-8') as f:
+    json.dump(employees, f)
