71513d3c472047ec5a7d9de6dead0205ca3fa65a
pero <djuro@nepostojecadomena.hr>
2023-11-22 18:48:05 +0100

    Add images to image_list

--- lab6/myimgor/images/views.py
+++ lab6/myimgor/images/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Image
 
 # Create your views here.
 def homepage(request):
-    context = {}
+    count = Image.objects.count()
+    context = {
+        "count": count,
+    }
     return render(request,'images/homepage.html', context)
 
 def image_list(request):
-    context = {}
+    images = Image.objects.all()
+    context = {
+        "images": images,
+    }
     return render(request, 
                 'images/image_list.html', 
                 context)
