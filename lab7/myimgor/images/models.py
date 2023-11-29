a3fac150da78820f421470682fdddda82b3d0a00
pero <djuro@nepostojecadomena.hr>
2023-11-29 18:03:18 +0100

    Add Comment model

--- lab7/myimgor/images/models.py
+++ lab7/myimgor/images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Image(models.Model):
@@ -9,3 +10,12 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    author = models.CharField(max_length=128)
+    content = models.CharField(max_length=512)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author}: {self.content[:30]}"
