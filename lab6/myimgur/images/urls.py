d5a08a0dbfa14cc0cfd00b460e578a4679e40cce
anas.enj <ana.senj13@gmail.com>
2023-12-01 14:00:59 +0100

    Add redirect and form for new image input

--- lab6/myimgur/images/urls.py
+++ lab6/myimgur/images/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import image_list,image_detail
+from .views import image_list,image_detail, image_create
 
 urlpatterns = [
     path('',image_list,name="image_list"),
     path('<int:image_id>/',image_detail,name="image_detail"),
+    path('new/',image_create,name="image_create"),
 ]
--- lab6/myimgur/images/views.py
+++ lab6/myimgur/images/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render,get_object_or_404
+from django.urls import reverse
 from .models import Image
 
 # Create your views here.
@@ -24,3 +25,21 @@ def image_detail(request,image_id):
         "image":image,
     }
     return render(request, 'images/image_detail.html',context)
+
+def image_create(request):
+    if request.method=="POST":
+        title=request.POST['title']
+        desc=request.POST['desc']
+        url=request.POST['url']
+        created_at=request.POST['created_at']
+        image=Image(
+            title=title,
+            desc=desc,
+            url=url,
+            created_at=created_at
+        )
+        image.save()
+        return HttpResponseRedirect(reverse('image_detail',args=(image.id,)))
+
+    context={}
+    return render(request,'images/image_create.html',context)
