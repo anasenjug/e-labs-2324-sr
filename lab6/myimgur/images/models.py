594751c363a741ecb59ff092f5be27b248d89d40
anas.enj <ana.senj13@gmail.com>
2023-12-01 12:34:10 +0100

    Add Image model and migrations

--- lab6/myimgur/images/models.py
+++ lab6/myimgur/images/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    title = models.CharField(max_length=128)
+    desc = models.CharField(max_length=512)
+    url = models.CharField(max_length=512)
+    created_at = models.DateTimeField()
+    
+
+
